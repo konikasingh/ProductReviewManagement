4d7f8728217a9e7f84deee585d5d45c723a3c25c
konika <konikasingh1996@gmail.com>
2021-12-10 11:18:16 +0530

    creating the class of product review

---
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 st method
+            //List<ProductReview> list = new List<ProductReview>();
+            //list.Add(new ProductReview() { ProductId = 1, UserId = 1, Review = "good", Rating = 17, IsLike = true });
+
+            //2 nd method
+            //Collection intializer
+            List<ProductReview> list = new List<ProductReview>()
+            {
+                new ProductReview(){ ProductId=1,UserId=1,Review="good",Rating=17,IsLike=true},
+                new ProductReview(){ ProductId=2,UserId=3,Review="bad",Rating=1,IsLike=false},
+                new ProductReview(){ ProductId=3,UserId=5,Review="good",Rating=20,IsLike=true},
+                new ProductReview(){ ProductId=4,UserId=7,Review="average",Rating=10,IsLike=true},
+                new ProductReview(){ ProductId=5,UserId=1,Review="bad",Rating=5,IsLike=false}
+            };
+        }
+    }
+}
